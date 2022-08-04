97c807c2f50c84fc43348d37e0fb0abd1977a7e6
Ummu Kalsum <108319934+ummukalsumfn@users.noreply.github.com>
2022-08-04 13:53:25 +0800

    Update 1 Data Preparation.py

--- Data Analyst Project: Business Decision Research/1 Data Preparation.py
+++ Data Analyst Project: Business Decision Research/1 Data Preparation.py
@@ -7,19 +7,3 @@ print(df.head())
 
 print('\nInfo dataset:') # Info dataset
 print(df.info())
-
-import pandas as pd
-df = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/data_retail.csv', sep=';')
-
-# Mengubah tipe data kolom First_Transaction dan Last_Transaction dari int ke datetime
-
-# Kolom First_Transaction
-df['First_Transaction'] = pd.to_datetime(df['First_Transaction']/1000, unit='s', origin='1970-01-01') 
-# Kolom Last_Transaction
-df['Last_Transaction']=pd.to_datetime(df['Last_Transaction']/1000,unit='s',origin='1970-01-01')
-
-print('Lima data teratas:')
-print(df.head())
-
-print('\nInfo dataset:')
-print(df.info())

7dcdd4f9d129357f5d0cf55adfef1143ed358244
Ummu Kalsum <108319934+ummukalsumfn@users.noreply.github.com>
2022-08-04 13:48:17 +0800

    Create 1 Data Preparation.py

---
+++ Data Analyst Project: Business Decision Research/1 Data Preparation.py
@@ -0,0 +1,9 @@
+import pandas as pd  #Import Library Pandas
+
+df = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/data_retail.csv',sep=';')
+
+print('Lima data teratas:') # Mencetak Lima data teratas
+print(df.head())
+
+print('\nInfo dataset:') # Info dataset
+print(df.info())
